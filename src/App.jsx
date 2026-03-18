d02d0bd5fd13ef089adeb8014d2a83cffe1d1501
Nicolae-Dan Nechifor <nicudannechifor@gmail.com>
2026-03-18 15:32:51 +0200

    ex5

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import Card from './Card';
 
 function App() {
+  const [count, setCount] = useState(0);
+
   const projects = [
     { title: 'Proiect 1', description: 'Pagina personala' },
     { title: 'Proiect 2', description: 'Calculator buget' },
@@ -14,6 +17,8 @@ function App() {
     <div>
       <h1>Dashboard</h1>
       <p>Nechifor Nicolae-Dan</p>
+      <p>Ai apasat de {count} ori</p>
+      <button onClick={() => setCount(count + 1)}>Click</button>
 
       {projects.map(function (item, index) {
         return <Card key={index} title={item.title} description={item.description} />;
