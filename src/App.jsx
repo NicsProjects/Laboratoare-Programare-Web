3f7d4cef6ea139b18d25a6c160975772b465a57d
Nicolae-Dan Nechifor <nicudannechifor@gmail.com>
2026-03-18 15:21:47 +0200

    app props

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,10 @@ function App() {
     <div>
     <h1>Dashboard</h1>
     <p>Nechifor Nicolae-Dan</p>
-    <Card />
+    
+    <Card title="Proiect 1" description="Pagina personala cu HTML si CSS" />
+    <Card title="Proiect 2" description="Pagina interactiva cu JavaScript" />
+    <Card title="Proiect 3" description="Dashboard cu React" />
     </div>
   );
 }
