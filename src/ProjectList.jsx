4527a206096d335b917520a759f5411c2cb03f9e
Nicolae-Dan Nechifor <nicudannechifor@gmail.com>
2026-04-15 13:35:43 +0300

    ex4

--- src/ProjectList.jsx
+++ src/ProjectList.jsx
@@ -37,6 +37,19 @@ function ProjectList() {
         return project.title.toLowerCase().includes(search.toLowerCase());
     });
 
+    const totalProjects = projects.length;
+    const completedProjects = projects.filter(function(project) {
+        return project.done;
+    }).length;
+    const inProgressProjects = projects.filter(function(project) {
+        return !project.done;
+    }).length;
+    const unfinishedProjects = projects.filter(function(project) {
+        return !project.done;
+    }).map(function(project) {
+                        return <li key={project.id}>{project.title}</li>;
+                    });
+
     return (
         <div>
             <h3>Proiecte</h3>
@@ -49,6 +62,13 @@ function ProjectList() {
             {filteredProjects.map(project => (
                 <Card key={project.id} title={project.title} description={project.description} />
             ))}
+            <div>
+                <h4>Statistici</h4>
+                <p>Total proiecte: {totalProjects}</p>
+                <p>Finalizate: {completedProjects}</p>
+                <p>In lucru: {inProgressProjects}</p>
+                <p>Neterminate: {unfinishedProjects.length}</p>
+            </div>
         </div>
     );
 }
