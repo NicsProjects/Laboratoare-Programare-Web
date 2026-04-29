d92c6b029779cbe7ccf772a1c5987aa08258946c
Nicolae-Dan Nechifor <nicudannechifor@gmail.com>
2026-04-29 12:48:18 +0300

    L7Ex5

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './Navbar';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
+import About from './pages/About';
 import NotFound from './pages/NotFound';
 import './App.css';
 
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>Sunt Nechifor Nicolae-Dan, student pasionat de programare și web development.</p>
+      <p>Mă interesează React, JavaScript și crearea de aplicații web moderne și interactive.</p>
+      <p>Prin acest proiect învăț React Router pentru a construi aplicații web cu mai multe pagini.</p>
+    </div>
+  );
+}
+
+export default About;
