d732cf142f4056df771d0b77e40bf9c3b8026f5c
Nicolae-Dan Nechifor <nicudannechifor@gmail.com>
2026-04-29 12:53:24 +0300

    L7Ex6

---
+++ src/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+  return (
+    <footer style={{
+      backgroundColor: '#2C3E50',
+      color: 'white',
+      padding: '20px',
+      textAlign: 'center',
+      marginTop: 'auto'
+    }}>
+      <p>&copy; 2026 Nechifor Nicolae-Dan. Toate drepturile rezervate.</p>
+      <p>Creat cu React și React Router</p>
+    </footer>
+  );
+}
+
+export default Footer;
