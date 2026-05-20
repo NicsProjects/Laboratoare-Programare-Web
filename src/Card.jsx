b5fa7f4b4033c48e6cb1ec5396eef611b8c26347
Nicolae-Dan Nechifor <nicudannechifor@gmail.com>
2026-05-20 13:37:47 +0300

    Lab11Ex5 - Stilizrează aplicația folosind CSS

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,16 +1,17 @@
 
 function Card(props) {
     return (
-        <div>
+        <div className={`project-card ${props.done ? 'done' : 'in-progress'}`}>
             <h3>{props.title}</h3>
             <p>{props.description}</p>
-            <p>Status: {props.done ? 'Finalizat' : 'In lucru'}</p>
-            <button onClick={props.onToggle}>
-                {props.done ? 'Marchează în lucru' : 'Marchează finalizat'}
-            </button>
-          {/*Buton de edit pe card: onClick*/}
-            <button onClick={props.onEdit}>Editează</button>
-            <button onClick={props.onDelete}>Șterge</button>
+            <p className="project-status">Status: {props.done ? 'Finalizat' : 'În lucru'}</p>
+            <div className="project-actions">
+                <button className="action-button toggle" onClick={props.onToggle}>
+                    {props.done ? 'Marchează în lucru' : 'Marchează finalizat'}
+                </button>
+                <button className="action-button edit" onClick={props.onEdit}>Editează</button>
+                <button className="action-button delete" onClick={props.onDelete}>Șterge</button>
+            </div>
         </div>
     );
 }
